e1e4a77b748542f2e1b4f7f46a395b2420bed9df
Stan Chang Khin Boon <khinboon@gmail.com>
2015-06-05 04:33:33 +0800

    Create the Xcode project.

---
+++ LXSupportKit/LXSupportKit.h
@@ -0,0 +1,19 @@
+//
+//  LXSupportKit.h
+//  LXSupportKit
+//
+//  Created by Stan Chang Khin Boon on 5/6/15.
+//  Copyright (c) 2015 lxcid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LXSupportKit.
+FOUNDATION_EXPORT double LXSupportKitVersionNumber;
+
+//! Project version string for LXSupportKit.
+FOUNDATION_EXPORT const unsigned char LXSupportKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LXSupportKit/PublicHeader.h>
+
+
