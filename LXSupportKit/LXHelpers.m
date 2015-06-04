83874f7d80c464c5b8224c93ddc67d62185d7a7d
Stan Chang Khin Boon <khinboon@gmail.com>
2015-06-05 05:21:01 +0800

    Use a more general type hint.

--- LXSupportKit/LXHelpers.m
+++ LXSupportKit/LXHelpers.m
@@ -9,12 +9,12 @@
 #import "LXHelpers.h"
 
 UIColor *LXRGBA(uint32_t x, CGFloat alpha) {
-    CGFloat b = (x & 0xff) / 255.0f; x >>= 8;
-    CGFloat g = (x & 0xff) / 255.0f; x >>= 8;
-    CGFloat r = (x & 0xff) / 255.0f;
+    CGFloat b = (x & 0xff) / 255.0; x >>= 8;
+    CGFloat g = (x & 0xff) / 255.0; x >>= 8;
+    CGFloat r = (x & 0xff) / 255.0;
     return [UIColor colorWithRed:r green:g blue:b alpha:alpha];
 }
 
 UIColor *LXRGB(uint32_t x) {
-    return LXRGBA(x, 1.0f);
+    return LXRGBA(x, 1.0);
 }
